2db5cad00ea475831bbfcd332712d68c4fe084cb
DanSch16 <DanSch16@users.noreply.github.com>
2018-12-10 13:56:13 +0100

    Added Vuforia package

---
+++ Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour {
+    public GameObject spherePrefab;
+	
+    public void NewSphereObject()
+    {
+        Instantiate(spherePrefab);
+    }
+}
---
+++ Assets/Scripts/SphereScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereScript : MonoBehaviour {
+
+    public float lat;
+    public float lon;
+    public string desc;
+
+    public void OnMouseDown()
+    {
+        Destroy(gameObject);    //gameObject this script is assigned to
+    }
+}
