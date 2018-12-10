4584c731443e5078e8d07e33741cd453848e1e86
DanSch16 <DanSch16@users.noreply.github.com>
2018-12-10 19:00:44 +0100

    Merged with Pauls scenes

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public void changemenu(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+    public void exitapp()
+    {
+        Debug.Log("Exit!");
+        Application.Quit();
+    }
+}
